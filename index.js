d86d3c4179c86dd7f56d910c236e7ce855311c54
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2020-10-19 20:56:13 -0300

    Criando formulário de perguntas

--- index.js
+++ index.js
@@ -11,4 +11,10 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/perguntar", (req,res) => {
+    res.render("perguntar",{
+
+    });
+});
+
 app.listen(8080, ()=>{console.log("App rodando!");});
