0471046866d9ef0de8a8c7b7ec6b1439b9f9dcce
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2020-10-19 20:15:08 -0300

    Adicionando Bootstrap
    Adicionando e configurando o Bootstrap no Projeto

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 
 // Configura o Express para usar o EJS como View Engine
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    
+    res.render("index",{
+    });
 });
 
 app.listen(8080, ()=>{console.log("App rodando!");});
