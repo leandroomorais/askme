d64329f6e299a00db7052b8853518612fdc6cffc
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2020-10-20 16:28:00 -0300

    Definindo a tabela pergunta com o Sequelize

---
+++ database/Pergunta.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+const connection = require("./database");
+
+//Definindo a tabela pergunta com o Sequelize
+const Pergunta = connection.define('pergunta', {
+    titulo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    descricao: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    }
+})
--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+//Database
+connection
+    .authenticate()
+    .then(() =>{
+        console.log("Conexão feita com o banco de dados")
+    })
+    .catch((msgErro) =>{
+        console.log(msgErro);
+    })
 
 // Configura o Express para usar o EJS como View Engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 // Configura a pasta 'public' como padrão 
 app.use(express.static('public'));
 // Configura a biblioteca body-parser para decodificar os dados enviados por formulários
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 // Configura d biblioteca body-parser para receber dados em JSON
 app.use(bodyParser.json());
 
 //Rotas
 app.get("/", (req, res) => {
-    
-    res.render("index",{
+
+    res.render("index", {
     });
 });
 
-app.get("/perguntar", (req,res) => {
-    res.render("perguntar",{
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar", {
     });
 });
 
 //Rota para salvar pergunta do formulário
-app.post("/salvar-pergunta", (req,res) => {
+app.post("/salvar-pergunta", (req, res) => {
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
     res.send("Dados recebidos" + "Titulo: " + titulo + "Descricao: " + descricao);
 });
 
-app.listen(8080, ()=>{console.log("App rodando!");});
+app.listen(8080, () => { console.log("App rodando!"); });
