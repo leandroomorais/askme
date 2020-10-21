d908d002e907e469e272cbbcc0910e262c46a226
Leandro Morais <38993942+leandroomorais@users.noreply.github.com>
2020-10-21 18:41:49 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = require('sequelize');
 
 const connection = new Sequelize('lknfdy3fejryob25','p6cuy4ybzg5rxrub','emvyx55mpzkl3fl9',{
+    port: '3306',
     host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
 });
